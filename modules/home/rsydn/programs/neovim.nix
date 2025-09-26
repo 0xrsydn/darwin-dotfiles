f9c294adae71a905efaa3aeee4d3928367128ce7
0xrsydn <rafayrussani@gmail.com>
2025-09-26 16:17:25 +0700

    chore:fix nix

--- modules/home/rsydn/programs/neovim.nix
+++ modules/home/rsydn/programs/neovim.nix
@@ -1,15 +1,16 @@
 { inputs, pkgs, ... }:
 let
   system = pkgs.stdenv.hostPlatform.system;
-  lazyvim = let nvimPackages = inputs."nvim-bundle".packages;
-  in if builtins.hasAttr system nvimPackages then
-    (builtins.getAttr system nvimPackages).neovim
+  nvimPackages = inputs."nvim-bundle".packages or { };
+  neovimUnwrapped = if builtins.hasAttr system nvimPackages then
+    let bundle = builtins.getAttr system nvimPackages;
+    in bundle.neovim-unwrapped or pkgs.neovim-unwrapped
   else
-    pkgs.neovim; # upstream bundle only exports x86_64-linux for now
+    pkgs.neovim-unwrapped;
 in {
   programs.neovim = {
     enable = true;
-    package = lazyvim;
+    package = neovimUnwrapped;
     withNodeJs = true;
     withPython3 = true;
   };
