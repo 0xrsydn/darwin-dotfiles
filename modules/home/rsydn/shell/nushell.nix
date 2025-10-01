305f8706e0c622741564840c06d1bad570a89d98
0xrsydn <rafayrussani@gmail.com>
2025-10-01 10:54:05 +0700

    fix: nushell oh my posh prompt config

--- modules/home/rsydn/shell/nushell.nix
+++ modules/home/rsydn/shell/nushell.nix
@@ -3,8 +3,31 @@ let
   themeName = "capr4n.omp.json";
   themeSource = ./oh-my-posh/capr4n.omp.json;
   themeConfigPath = "${config.xdg.configHome}/oh-my-posh/${themeName}";
+  profileBin = "${config.home.profileDirectory}/bin";
+  systemBin = "/run/current-system/sw/bin";
+  defaultBin = "/nix/var/nix/profiles/default/bin";
 in {
   programs.nushell.enable = lib.mkDefault true;
+
+  programs.nushell.envFile.text = ''
+    let nix_paths = [
+      "${profileBin}"
+      "${systemBin}"
+      "${defaultBin}"
+    ]
+
+    if ($env.PATH? == null) {
+      $env.PATH = $nix_paths
+    } else {
+      for path in $nix_paths {
+        if not ($env.PATH | any {|it| $it == $path}) {
+          $env.PATH = ($env.PATH | append $path)
+        }
+      }
+    }
+
+    $env.NIX_PROFILES = "/run/current-system/sw ${config.home.profileDirectory}"
+  '';
 
   programs.oh-my-posh = {
     enable = lib.mkDefault true;
